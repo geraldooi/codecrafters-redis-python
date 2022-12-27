999d78f6cdcc28659014fafd0782a2530b2a9918
Gerald Ooi <27208744+geraldooi@users.noreply.github.com>
2022-12-27 18:28:29 +0800

    connection reply PONG to client

--- app/main.py
+++ app/main.py
@@ -9,7 +9,9 @@ def main():
     # Uncomment this to pass the first stage
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    conn, _ = server_socket.accept()  # wait for client
+
+    conn.send(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
